ec963a0f309dd8b9956ba980823bca5f890b5042
Shoti Chinchaladze <Shoti.chinchala@gmail.com>
2021-12-20 21:40:14 +0400

    get logged in user

--- src/MiaCore/Features/UpdateProfile/UpdateProfileRequestHandler.cs
+++ src/MiaCore/Features/UpdateProfile/UpdateProfileRequestHandler.cs
@@ -5,6 +5,7 @@ using AutoMapper;
 using MediatR;
 using MiaCore.Infrastructure.Persistence;
 using MiaCore.Models;
+using MiaCore.Utils;
 using Microsoft.Extensions.Options;
 
 namespace MiaCore.Features.UpdateProfile
@@ -14,17 +15,20 @@ namespace MiaCore.Features.UpdateProfile
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
         private readonly MiaCoreOptions _options;
+        private readonly UserHelper _userHepler;
 
-        public UpdateProfileRequestHandler(IUserRepository userRepository, IMapper mapper, IOptions<MiaCoreOptions> options)
+        public UpdateProfileRequestHandler(IUserRepository userRepository, IMapper mapper, IOptions<MiaCoreOptions> options, UserHelper userHepler)
         {
             _userRepository = userRepository;
             _mapper = mapper;
             _options = options?.Value ?? throw new ArgumentNullException(nameof(options));
+            _userHepler = userHepler;
         }
 
         public async Task<MiaUserDto> Handle(UpdateProfileRequest request, CancellationToken cancellationToken)
         {
             var user = _mapper.Map<MiaUser>(request);
+            user.Id = _userHepler.GetUserId();
 
             await _userRepository.UpdateAsync(user);
 
