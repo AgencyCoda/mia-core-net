0ddfa96d314f95ecfc3cbca1685fec29f2bd5669
Shoti Chinchaladze <Shoti.chinchala@gmail.com>
2022-07-22 17:38:47 +0400

    update profile add new fields

--- src/MiaCore/Features/UpdateProfile/UpdateProfileRequest.cs
+++ src/MiaCore/Features/UpdateProfile/UpdateProfileRequest.cs
@@ -10,5 +10,7 @@ namespace MiaCore.Features.UpdateProfile
         public string Photo { get; set; }
         public string Phone { get; set; }
         public string Caption { get; set; }
+        public string Language { get; set; }
+        public bool OtpEnabled { get; set; }
     }
 }
